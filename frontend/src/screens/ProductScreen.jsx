ed6d888949ff63baa9895c6014da0c5b1d9f1f38
Zhanhu079 <keitumetse1233@gmail.com>
2023-10-27 14:42:34 +0200

    Add Rating component and ProductScreen

---
+++ frontend/src/screens/ProductScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+import products from '../products';
+
+const ProductScreen = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProductScreen
