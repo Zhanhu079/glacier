e58c77be8f6f64b7ef965745f4482709b5ff3b7a
Zhanhu079 <keitumetse1233@gmail.com>
2023-11-04 11:45:52 +0200

    Add comments

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -3,14 +3,16 @@ const router = express.Router();
 import asyncHandler from "../middleware/asyncHandler.js";
 import Product from '../models/productModel.js';
 
+// /api/products/
 router.get('/', asyncHandler(async (req, res) => {
     const products = await Product.find({});
     res.json(products);
 }));
 
+// /api/products/{objectID} 
 router.get('/:id', asyncHandler(async (req, res) => {
     const product = await Product.findById(req.params.id);
-    
+
     if (product) {
         return res.json(product);
     }
