bbddb623ad3b78f703caae3719e1143966e13e93
Zhanhu079 <keitumetse1233@gmail.com>
2023-12-15 20:07:27 +0200

    User register endpoint and encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -26,7 +26,18 @@ const userSchema = new mongoose.Schema({
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
-}
+};
+
+// pre lets us do something before it's saved in the database
+// save is our method
+userSchema.pre('save', async function (next) { 
+    if (!this.isModified('password')) {
+        next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
 
 const User = mongoose.model("User", userSchema);
 
