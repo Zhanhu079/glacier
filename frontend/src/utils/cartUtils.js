2be111b303368a4e28dd1aa9cd2d489d1de58d58
Zhanhu079 <keitumetse1233@gmail.com>
2023-12-16 16:12:25 +0200

    Convert prices again

--- frontend/src/utils/cartUtils.js
+++ frontend/src/utils/cartUtils.js
@@ -6,8 +6,8 @@ export const updateCart = (state) => {
     // Calculate items price 
     state.itemsPrice = addDecimals(state.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0));
 
-    // Calculate shipping price (If order is over $100 then free, else $10 shipping)
-    state.shippingPrice = addDecimals(state.itemsPrice > 100 ? 0 : 10);
+    // Calculate shipping price (If order is over R100 then free, else R70 shipping)
+    state.shippingPrice = addDecimals(state.itemsPrice > 400 ? 0 : 70);
 
     // Calculate tax price (15% tax)
     state.taxPrice = addDecimals(Number((0.15 * state.itemsPrice).toFixed(2)));
