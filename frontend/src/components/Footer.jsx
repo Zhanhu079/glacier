ca9dfee4325bf67f0e904f94f8a2d1ffa92146a7
Zhanhu079 <keitumetse1233@gmail.com>
2023-12-18 20:45:32 +0200

    Modify footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,11 +4,11 @@ import { Container, Row, Col } from "react-bootstrap";
 const Footer = () => {
     const currentYear = new Date().getFullYear();
   return (
-    <footer>
+    <footer className='mt-3 main-bg'>
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            <p>Glacier &copy; {currentYear}</p>
+            <p>Glacier &copy; {currentYear}, Design by Keitumetse Sello</p>
           </Col>
         </Row>
       </Container>
